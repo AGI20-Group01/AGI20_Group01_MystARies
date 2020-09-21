920c9d2ce5b11732958b8c4c9f8769ff27e10946
Jacob <jacobel@kth.se>
2020-09-21 17:09:34 +0200

    Compiled everything with grid into a demo

---
+++ AGI20/Assets/Scripts/Grid System/Grid_AnimationExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid_AnimationExample : MonoBehaviour
+{
+    public Vector3 destination;
+    public GameObject PlayerTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, destination, 5f * Time.deltaTime);
+        destination = PlayerTracker.transform.position;
+    }
+}
