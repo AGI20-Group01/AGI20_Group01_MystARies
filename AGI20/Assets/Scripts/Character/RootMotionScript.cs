cd54f44d14c6de65b41797b1a92461954fce5ddf
MachiRic <mrich@kth.se>
2020-09-23 09:11:42 +0200

    First version of Traveler done
    Working animation interaction through Unity. For now we need a better way for the user to control the traveler. Best solution for now is probably touch input from phone or just by using the keyboard.

---
+++ AGI20/Assets/Scripts/Character/RootMotionScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+    
+public class RootMotionScript : MonoBehaviour {
+            
+    void OnAnimatorMove()
+    {
+            Animator animator = GetComponent<Animator>(); 
+                              
+            if (animator)
+            {
+                Debug.Log(animator);
+     Vector3 newPosition = transform.position;
+               newPosition.z += animator.GetFloat("vertical") * Time.deltaTime; 
+               newPosition.x += animator.GetFloat("horizontal") * Time.deltaTime;
+     transform.position = newPosition;
+            }
+    }
+}
