a0a852d945f0d7a242847c4000a83ed54506a834
MachiRic <mrich@kth.se>
2020-09-24 17:23:31 +0200

    Updated interaction and animation
    It's possible to control the character using the keyboard and the animation has been updated so that it works better when the traveler moves backwards.

---
+++ AGI20/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Animator anim;
+    
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        anim.SetFloat("horizontal",h);
+        float v = Input.GetAxis("Vertical");
+        anim.SetFloat("vertical",v);
+
+        if (h!=0||v!=0 )
+        {
+            anim.SetBool("Running", true);
+        }
+        else 
+        {
+            anim.SetBool("Running", false);
+        }
+        
+    }
+}
+
+
