821db92dc4a26250d66c67d5506fc31270e26c70
Jacob <jacobel@kth.se>
2020-09-26 19:38:12 +0200

    Started world spawner, touch plane to place world

---
+++ AGI20/Assets/Scripts/AR/ARCreateWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCreateWorld : MonoBehaviour
+{
+    public GameObject worldObject;
+    private ARCursorScript cursorIndicator;
+
+    private void Start()
+    {
+        cursorIndicator = FindObjectOfType<ARCursorScript>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(worldObject, cursorIndicator.transform.position, cursorIndicator.transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ AGI20/Assets/Scripts/Worlds/SpawnWorld.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SpawnWorld : MonoBehaviour
+{
+    public GameObject worldSpawner;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+    private bool objectPlaced = false;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            if (objectPlaced)
+            {
+                this.enabled = false;
+            }
+            return;
+        }
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            objectPlaced = true;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(worldSpawner, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
