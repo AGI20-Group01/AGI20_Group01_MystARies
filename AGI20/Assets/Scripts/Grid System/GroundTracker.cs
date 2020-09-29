8891b2c3caf39d766051917b5053532d481410bc
Emil Clemedson <emilcle@gmail.com>
2020-09-29 11:17:18 +0200

    fix grid in editor

--- AGI20/Assets/Scripts/Grid System/GroundTracker.cs
+++ AGI20/Assets/Scripts/Grid System/GroundTracker.cs
@@ -1,62 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine; 
 
 public class GroundTracker : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Dictionary<Vector3, GameObject> ground = new Dictionary<Vector3, GameObject>();
     private Grid grid;
-    public GameObject[] groundTypes;
+    public GameObject[] groudTypes;
     public Transform theGround;
-
-
-    /*public Slider x;
-    public Slider y;
-    public Slider z;
-    
-    public Slider type;
-    public Slider op;
-    */
-
 
     void Start() {
         grid = FindObjectOfType<Grid>();
-        int id = 1;
-        foreach(Transform child in theGround) {
-            Vector3 pos = child.position;
-            pos = grid.GetNearestPointOnGrid(pos);
-            if (ground.ContainsKey(pos)) {
-                Destroy(child.gameObject);
-                return;
-            }
-            updateCubesSurrounding(child.gameObject, pos);
+        snapAllOnjects(theGround, ground);
+    }
 
-            ground.Add(pos, child.gameObject);
-            child.position = pos;
-            id++;
+    public void snapAllOnjects() {
+        grid = FindObjectOfType<Grid>();
+        snapAllOnjects(theGround, new Dictionary<Vector3, GameObject>());
+    }
+
+    private void snapAllOnjects(Transform parent,  Dictionary<Vector3, GameObject> ground) {
+        foreach(Transform child in parent) {
+            if (child.tag != "GroundeCube" && child.tag != "interactablecube") {
+                snapAllOnjects(child, ground);
+            } else {
+                snapObject(child, ground);
+            }
         }
     }
 
-    private void updateCubesSurrounding(GameObject go, Vector3 pos) {
-        GroundCube gc = go.GetComponent<GroundCube>();
-        if (ground.ContainsKey(pos + new Vector3(0,1,0)))
-                gc.SetUnder(true);
+    private void snapObject(Transform transform, Dictionary<Vector3, GameObject> ground) {
+        Vector3 pos = transform.position;
+        pos = grid.GetNearestPointOnGrid(pos);
+        if (ground.ContainsKey(pos)) {
+            DestroyImmediate(transform.gameObject);
+            return;
+        }
 
-        if (ground.ContainsKey(pos + new Vector3(0,-1,0)))
+        updateCubesSurrounding(transform.gameObject, pos, ground);
+
+        ground.Add(pos, transform.gameObject);
+        transform.position = pos;
+    } 
+
+
+    private void updateCubesSurrounding(GameObject go, Vector3 pos, Dictionary<Vector3, GameObject> ground) {
+        GroundCube gc = go.GetComponent<GroundCube>();
+        if (ground.ContainsKey(pos + new Vector3(0,1,0))) {
+            gc.SetUnder(true);
+        } else {
+            gc.SetUnder(false);
+        }
+
+        if (ground.ContainsKey(pos + new Vector3(0,-1,0))) {
             ground[(pos + new Vector3(0,-1,0))].GetComponent<GroundCube>().SetUnder(true);
+        }
 
     }
-    //*****
+
     public void AddCube(Vector3 pos, int type) {
         Vector3 gridPos = grid.GetNearestPointOnGrid(pos);
 
         if (ground.ContainsKey(gridPos)) {
             return;
         }
-        GameObject go = Instantiate(groundTypes[type], gridPos, Quaternion.identity);
-        updateCubesSurrounding(go, gridPos);
+        GameObject go = Instantiate(groudTypes[type], gridPos, Quaternion.identity);
+        updateCubesSurrounding(go, gridPos, ground);
         ground.Add(gridPos, go);
     }
 
@@ -70,28 +80,5 @@ public class GroundTracker : MonoBehaviour
                 ground[(gridPos + new Vector3(0,-1,0))].GetComponent<GroundCube>().SetUnder(false);
         }
     }
-    //******
-
-    /*public void test() {
-        float fx = x.value;
-        float fy = y.value;
-        float fz = z.value;
-        int ft = (int) type.value;
-        float fop = (int) op.value;
-
-        if (fop == 1)
-            AddCube(new Vector3(fx,fy,fz), ft);
-        else
-            RemoveCube(new Vector3(fx,fy,fz));
-    }
-
-    private void OnDrawGizmos()
-    {
-        float fx = x.value;
-        float fy = y.value;
-        float fz = z.value;
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(new Vector3(fx, fy, fz), 0.1f);
-
-    }*/
+   
 }
