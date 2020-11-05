db6c99232d216dc9cea9b8d67ceaef72d3b1fb8c
Jacob <jacobel@kth.se>
2020-11-05 10:13:28 +0100

    inventory and haptics

---
+++ AGI20/Assets/Scripts/Inventory/UIInventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    public List<UIItem> uiItems = new List<UIItem>();
+    public GameObject slotPrefab;
+    public Transform slotPanel;
+
+    void Awake()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            GameObject instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+            uiItems.Add(instance.GetComponentInChildren<UIItem>());
+        }
+    }
+
+    public void UpdateSlot(int slot, BlockSciptableObject block)
+    {
+        uiItems[slot].UpdateItem(block);
+    }
+
+    public void AddNewItem(BlockSciptableObject block)
+    {
+        UpdateSlot(uiItems.FindIndex(i => i.block == null), block);
+    }
+
+    public void RemoveItem(BlockSciptableObject block)
+    {
+        UpdateSlot(uiItems.FindIndex(i => i.block == block), null);
+    }
+}
---
+++ AGI20/Assets/Scripts/Inventory/UIItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIItem : MonoBehaviour, IPointerDownHandler
+{
+    public BlockSciptableObject block;
+    private Image spriteImage;
+    private UIItem selectedItem;
+    void Awake()
+    {
+        selectedItem = GameObject.Find("SelectedItem").GetComponent<UIItem>();
+        spriteImage = GetComponent<Image>();
+        UpdateItem(null);
+    }
+
+    public void UpdateItem(BlockSciptableObject block)
+    {
+        this.block = block;
+        if (this.block != null)
+        {
+            spriteImage.color = Color.white;
+            spriteImage.sprite = block.icon;
+        }
+        else
+        {
+            spriteImage.color = Color.clear;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (this.block != null)
+        {
+            if (selectedItem.block != null)
+            {
+                BlockSciptableObject clone = new BlockSciptableObject(selectedItem.block);
+                selectedItem.UpdateItem(this.block);
+                UpdateItem(clone);
+            }
+            else
+            {
+                selectedItem.UpdateItem(this.block);
+                UpdateItem(null);
+            }
+        }
+        else if (selectedItem.block != null)
+        {
+            UpdateItem(selectedItem.block);
+            selectedItem.UpdateItem(null);
+        }
+    }
+}
