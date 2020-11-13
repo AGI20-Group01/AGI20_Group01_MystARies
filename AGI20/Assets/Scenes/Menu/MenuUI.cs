7121d237c3314876460a586bb312d810ea2b8477
Jacob <jacobel@kth.se>
2020-11-13 15:26:28 +0100

    loader done

---
+++ AGI20/Assets/Scenes/Menu/MenuUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+    public void startSpirit()
+    {
+        SceneHandler.Load(SceneHandler.Scene.SpiritScene);
+    }
+
+    public void startTraveller()
+    {
+        SceneHandler.Load(SceneHandler.Scene.TravellerAR);
+    }
+}
