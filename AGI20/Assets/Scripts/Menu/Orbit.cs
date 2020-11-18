4e77aec8e656754fda1e8e695eb4e5bcc79436ce
Josefin Ström <hedvigjosefin@gmail.com>
2020-11-18 16:56:25 +0100

    Backsplash animated
    Need to fix orbits

---
+++ AGI20/Assets/Scripts/Menu/Orbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{/* the object to orbit */
+    public Transform target;
+    public Vector3 axis;
+
+    /* speed of orbit (in degrees/second) */
+    public float speed;
+
+    public void Update()
+    {
+        if (target != null)
+        {
+            transform.RotateAround(target.position, axis, speed * Time.deltaTime);
+        }
+    }
+}
