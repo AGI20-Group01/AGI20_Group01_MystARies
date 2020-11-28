a8f2a2c536ac5b06a275486ee5ae6f1233561e9a
Jacob <jacobel@kth.se>
2020-11-28 21:33:27 +0100

    wip collision with collectable

---
+++ AGI20/Assets/Scripts/Traveler/Indicator/IndicatorWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorWindow : MonoBehaviour
+{
+    private static IndicatorWindow instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static void AddIndicator(Vector3 pos)
+    {
+        Transform IndicatorUI = Instantiate(GameAssets.i.IndicatorUI, instance.transform);
+        IndicatorUI.GetComponent<OnScreenIndicator>().setUp(pos);
+    }
+}
---
+++ AGI20/Assets/Scripts/Traveler/Indicator/startIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startIndicator : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    void Awake()
+    {
+        IndicatorWindow.AddIndicator(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScorePointSystem.score += 1;
+        Destroy(gameObject);
+    }
+}
